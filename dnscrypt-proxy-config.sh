e547d2f5e3d1d945a66025cd867b3a7194412601
chinchun <zhangjj129@gmail.com>
2022-09-17 16:14:38 +0800

    modified:   dnscrypt-proxy-config.sh modified:   dnscrypt-proxy.toml

--- dnscrypt-proxy-config.sh
+++ dnscrypt-proxy-config.sh
@@ -1,42 +1,38 @@
-fetch -m https://github.com/felixonmars/dnsmasq-china-list/raw/master/accelerated-domains.china.conf
-echo '# Converted from https://github.com/felixonmars/dnsmasq-china-list/blob/master/accelerated-domains.china.conf' >dnscrypt-forwarding-rules.txt
-echo '# https://github.com/felixonmars/dnsmasq-china-list' >>dnscrypt-forwarding-rules.txt
-echo '# Thanks to all contributors.' >>dnscrypt-forwarding-rules.txt
-echo '' >>dnscrypt-forwarding-rules.txt
-cat accelerated-domains.china.conf | grep -v '^#server' | sed -e 's|/| |g' -e 's|^server= ||' | sed 's/114.114.114.114/223.5.5.5,119.29.29.29/g' >>dnscrypt-forwarding-rules.txt
+fetch https://github.com/felixonmars/dnsmasq-china-list/raw/master/accelerated-domains.china.conf
+echo '# Converted from https://github.com/felixonmars/dnsmasq-china-list/blob/master/accelerated-domains.china.conf' >/tmp/dnscrypt-forwarding-rules.txt
+echo '# https://github.com/felixonmars/dnsmasq-china-list' >>/tmp/dnscrypt-forwarding-rules.txt
+echo '# Thanks to all contributors.' >>/tmp/dnscrypt-forwarding-rules.txt
+echo '' >>/tmp/dnscrypt-forwarding-rules.txt
+cat /tmp/accelerated-domains.china.conf | grep -v '^#server' | sed -e 's|/| |g' -e 's|^server= ||' | sed 's/114.114.114.114/223.5.5.5,119.29.29.29/g' >>/tmp/dnscrypt-forwarding-rules.txt
 
-fetch -m https://github.com/felixonmars/dnsmasq-china-list/raw/master/bogus-nxdomain.china.conf
-echo '# Converted from https://github.com/felixonmars/dnsmasq-china-list/blob/master/bogus-nxdomain.china.conf' >dnscrypt-blacklist-ips.txt
-echo '# https://github.com/felixonmars/dnsmasq-china-list' >>dnscrypt-blacklist-ips.txt
-echo '# Thanks to all contributors.' >>dnscrypt-blacklist-ips.txt
-echo '' >>dnscrypt-blacklist-ips.txt
-cat bogus-nxdomain.china.conf | grep -v '^#bogus' | grep bogus-nxdomain | sed 's/bogus-nxdomain=//g' >>dnscrypt-blacklist-ips.txt
+fetch https://github.com/felixonmars/dnsmasq-china-list/raw/master/bogus-nxdomain.china.conf
+echo '# Converted from https://github.com/felixonmars/dnsmasq-china-list/blob/master/bogus-nxdomain.china.conf' >/tmp/dnscrypt-blacklist-ips.txt
+echo '# https://github.com/felixonmars/dnsmasq-china-list' >>/tmp/dnscrypt-blacklist-ips.txt
+echo '# Thanks to all contributors.' >>/tmp/dnscrypt-blacklist-ips.txt
+echo '' >>/tmp/dnscrypt-blacklist-ips.txt
+cat /tmp/bogus-nxdomain.china.conf | grep -v '^#bogus' | grep bogus-nxdomain | sed 's/bogus-nxdomain=//g' >>/tmp/dnscrypt-blacklist-ips.txt
 
-fetch -m https://github.com/missdeer/blocklist/raw/master/toblock-without-shorturl-optimized.lst
-echo '# Converted from https://github.com/missdeer/blocklist/blob/master/toblock-without-shorturl-optimized.lst' >dnscrypt-blacklist-domains.txt
-echo '# https://github.com/missdeer/blocklist' >>dnscrypt-blacklist-domains.txt
-echo '# Thanks to all contributors.' >>dnscrypt-blacklist-domains.txt
-echo '' >>dnscrypt-blacklist-domains.txt
-echo 'ad.*' >>dnscrypt-blacklist-domains.txt
-echo 'ad[0-9]*' >>dnscrypt-blacklist-domains.txt
-echo 'ads.*' >>dnscrypt-blacklist-domains.txt
-echo 'ads[0-9]*' >>dnscrypt-blacklist-domains.txt
-cat toblock-without-shorturl-optimized.lst | grep -v '^#' | tr -s '\n' | tr A-Z a-z | grep -v '^ad\.' | grep -v -e '^ad[0-9]' | grep -v '^ads\.' | grep -v -e '^ads[0-9]' | rev | sort -n | uniq | rev >>dnscrypt-blacklist-domains.txt
+fetch https://github.com/missdeer/blocklist/raw/master/toblock-without-shorturl-optimized.lst
+echo '# Converted from https://github.com/missdeer/blocklist/blob/master/toblock-without-shorturl-optimized.lst' >/tmp/dnscrypt-blacklist-domains.txt
+echo '# https://github.com/missdeer/blocklist' >>/tmp/dnscrypt-blacklist-domains.txt
+echo '# Thanks to all contributors.' >>/tmp/dnscrypt-blacklist-domains.txt
+echo '' >>/tmp/dnscrypt-blacklist-domains.txt
+cat /tmp/toblock-without-shorturl-optimized.lst | grep -v '^#' | tr -s '\n' | tr A-Z a-z | grep -v '^ad\.' | grep -v -e '^ad[0-9]' | grep -v '^ads\.' | grep -v -e '^ads[0-9]' | rev | sort -n | uniq | rev >>/tmp/dnscrypt-blacklist-domains.txt
 
-fetch -m https://github.com/googlehosts/hosts/raw/master/hosts-files/dnscrypt-proxy-cloaking.txt
-echo '# Converted from https://github.com/googlehosts/hosts/blob/master/hosts-files/dnscrypt-proxy-cloaking.txt' >dnscrypt-cloaking-rules.txt
-echo '# https://github.com/googlehosts/hosts' >>dnscrypt-cloaking-rules.txt
-echo '# Thanks to all contributors.' >>dnscrypt-cloaking-rules.txt
-echo '' >>dnscrypt-cloaking-rules.txt
-cat dnscrypt-proxy-cloaking.txt >>dnscrypt-cloaking-rules.txt
+fetch https://github.com/googlehosts/hosts/raw/master/hosts-files/dnscrypt-proxy-cloaking.txt
+echo '# Converted from https://github.com/googlehosts/hosts/blob/master/hosts-files/dnscrypt-proxy-cloaking.txt' >/tmp/dnscrypt-cloaking-rules.txt
+echo '# https://github.com/googlehosts/hosts' >>/tmp/dnscrypt-cloaking-rules.txt
+echo '# Thanks to all contributors.' >>/tmp/dnscrypt-cloaking-rules.txt
+echo '' >>/tmp/dnscrypt-cloaking-rules.txt
+cat /tmp/dnscrypt-proxy-cloaking.txt >>/tmp/dnscrypt-cloaking-rules.txt
 
-rm accelerated-domains.china.conf bogus-nxdomain.china.conf toblock-without-shorturl-optimized.lst dnscrypt-proxy-cloaking.txt
+rm /tmp/accelerated-domains.china.conf /tmp/bogus-nxdomain.china.conf /tmp/toblock-without-shorturl-optimized.lst /tmp/dnscrypt-proxy-cloaking.txt
 
-fetch -m https://raw.githubusercontent.com/jedisct1/dnscrypt-proxy/master/dnscrypt-proxy/example-dnscrypt-proxy.toml
-fetch -m https://raw.githubusercontent.com/DNSCrypt/dnscrypt-proxy/master/dnscrypt-proxy/example-captive-portals.txt -O dnscrypt-captive-portals.txt
-fetch -m https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/odoh-relays.md
-fetch -m https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/odoh-servers.md
-fetch -m https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/parental-control.md
-fetch -m https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md
-fetch -m https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/relays.md
-fetch -m https://www.quad9.net/quad9-resolvers.md
+fetch https://raw.githubusercontent.com/jedisct1/dnscrypt-proxy/master/dnscrypt-proxy/example-dnscrypt-proxy.toml
+fetch -o dnscrypt-captive-portals.txt https://raw.githubusercontent.com/DNSCrypt/dnscrypt-proxy/master/dnscrypt-proxy/example-captive-portals.txt
+fetch https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/odoh-relays.md
+fetch https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/odoh-servers.md
+fetch https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/parental-control.md
+fetch https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md
+fetch https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/relays.md
+fetch https://www.quad9.net/quad9-resolvers.md
